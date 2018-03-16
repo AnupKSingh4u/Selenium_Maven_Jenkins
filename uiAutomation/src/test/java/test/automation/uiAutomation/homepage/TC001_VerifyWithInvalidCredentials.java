b9df074a889d88093055b9115372129c61e99e85
AKSingh4u <30553501+AKSingh4u@users.noreply.github.com>
2018-03-16 11:32:18 +0530

    Git change code

--- uiAutomation/src/test/java/test/automation/uiAutomation/homepage/TC001_VerifyWithInvalidCredentials.java
+++ uiAutomation/src/test/java/test/automation/uiAutomation/homepage/TC001_VerifyWithInvalidCredentials.java
@@ -18,6 +18,7 @@ public class TC001_VerifyWithInvalidCredentials extends TestBase {
 	@Test(priority=1)
 	public void setUp() {
 		init("chrome","http://automationpractice.com/index.php");
+		System.out.println("GIT Message for change");
 	}
 	
 	@Test(priority=2)
